7768b162cc58f56a235cce91963948e6c1990f6f
honnanes1 <honnanes1@gmail.com>
2024-02-18 19:24:12 +0100

    rows

--- Module 3 /bigquery/basiq_bigquery.sql
+++ Module 3 /bigquery/basiq_bigquery.sql
@@ -5,8 +5,14 @@ uris = ['gs://module-3-zoomcamp/yellow/yellow_tripdata_2020-*.parquet',
     'gs://module-3-zoomcamp/yellow/yellow_tripdata_2019-*.parquet']);
 
 
-    -- change the datetime to TIMESTAMP
+-- change the datetime to TIMESTAMP
     CREATE OR REPLACE TABLE de-zoomcamp-412301.ny_taxi.external_yellow_tripdata_corrected AS 
     SELECT *, PARSE_DATETIME('%Y-%m-%d %H:%M:%S', tpep_pickup_datetime) AS tpep_pickup_datetime
     FROM de-zoomcamp-412301.ny_taxi.external_yellow_tripdata;
-    
+
+-- select 10 rows to see 
+SELECT * FROM `ny_taxi.external_yellow_tripdata_corrected`
+ORDER BY pickup_date  ASC
+LIMIT 500 ;
+
+
