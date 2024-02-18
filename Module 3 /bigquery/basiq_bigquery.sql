1633c4681cd7691c401f023abc49af417a1a4dfd
honnanes1 <honnanes1@gmail.com>
2024-02-18 19:20:36 +0100

    change datetime to timestamp

--- Module 3 /bigquery/basiq_bigquery.sql
+++ Module 3 /bigquery/basiq_bigquery.sql
@@ -3,4 +3,10 @@ CREATE or REPLACE EXTERNAL TABLE `de-zoomcamp-412301.ny_taxi.external_yellow_tri
 OPTIONS( format = 'parquet',
 uris = ['gs://module-3-zoomcamp/yellow/yellow_tripdata_2020-*.parquet',
     'gs://module-3-zoomcamp/yellow/yellow_tripdata_2019-*.parquet']);
+
+
+    -- change the datetime to TIMESTAMP
+    CREATE OR REPLACE TABLE de-zoomcamp-412301.ny_taxi.external_yellow_tripdata_corrected AS 
+    SELECT *, PARSE_DATETIME('%Y-%m-%d %H:%M:%S', tpep_pickup_datetime) AS tpep_pickup_datetime
+    FROM de-zoomcamp-412301.ny_taxi.external_yellow_tripdata;
     
