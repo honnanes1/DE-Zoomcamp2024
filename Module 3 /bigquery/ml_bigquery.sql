708e357f8173fef93c0ded09527c0eb1ffd4b638
honnanes1 <honnanes1@gmail.com>
2024-02-21 18:28:45 +0100

    explain

--- Module 3 /bigquery/ml_bigquery.sql
+++ Module 3 /bigquery/ml_bigquery.sql
@@ -59,3 +59,35 @@ FROM
 WHERE
 tip_amount IS NOT NULL
 ));
+
+-- Predict and explain
+SELECT
+*
+FROM
+ML.EXPLAIN_PREDICT(MODEL `de-zoomcamp-412301.ny_taxi.tip_model`,
+(
+SELECT
+*
+FROM
+`de-zoomcamp-412301.ny_taxi.yellow_tripdata_ml`
+WHERE
+tip_amount IS NOT NULL
+), STRUCT(3 as top_k_features));
+
+
+-- HYPER PARAM TUNNING
+CREATE OR REPLACE MODEL `de-zoomcamp-412301.ny_taxi.tip_hyperparam_model`
+OPTIONS
+(model_type='linear_reg',
+input_label_cols=['tip_amount'],
+DATA_SPLIT_METHOD='AUTO_SPLIT',
+num_trials=5,
+max_parallel_trials=2,
+l1_reg=hparam_range(0, 20),
+l2_reg=hparam_candidates([0, 0.1, 1, 10])) AS
+SELECT
+*
+FROM
+`de-zoomcamp-412301.ny_taxi.yellow_tripdata_ml`
+WHERE
+tip_amount IS NOT NULL;
